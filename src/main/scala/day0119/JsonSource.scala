8f0b85ce327aa505d9d48a7a9c8ef7b9657f712c
tosin <TosinJia@foxmail.com>
2019-01-20 15:30:21 +0800

    json作为数据源

---
+++ src/main/scala/day0119/JsonSource.scala
@@ -0,0 +1,20 @@
+package day0119
+
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+
+object JsonSource {
+  def main(args: Array[String]): Unit = {
+    // 1 创建sparkSession
+    val sparkSession: SparkSession = SparkSession.builder().appName("JsonSource")
+      .master("local[2]").getOrCreate()
+    // 2 读取json数据源
+    val dataFrame: DataFrame = sparkSession.read.json("/Users/tosin/Documents/IdeaProjects/spark-test/test-data/saveJson")
+    // 3 处理数据
+    import sparkSession.implicits._
+    val dataSet: Dataset[Row] = dataFrame.filter($"xueyuan"==="java")
+    // 4 触发action
+    dataSet.show()
+    // 5 关闭资源
+    sparkSession.stop()
+  }
+}
